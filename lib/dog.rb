1a9d241efbda406b482769a963dce59099227cdf
Courtney DeRiggs <courtney.deriggs@gmail.com>
2020-02-06 16:30:45 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,6 @@
+#=> initialize: set dog name to @name
+class Dog
+  def initialize(breed)
+    @breed= breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+#=> initialize: set person name to @name
+class Person
+  def initialize(name)
+    @name = name
+  end
+end
